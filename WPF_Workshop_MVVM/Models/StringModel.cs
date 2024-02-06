ec919f003459f93017ff297e073b992b618e7908
Arenz <if.arenz@gmail.com>
2024-02-06 14:47:06 +0100

    doesn't work

---
+++ WPF_Workshop_MVVM/Models/StringModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WPF_Workshop_MVVM.Models
+{
+    public class StringModel
+    {
+        public string ToUpperCase(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
---
+++ WPF_Workshop_MVVM/ViewModels/StringViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using WPF_Workshop_MVVM.Models;
+
+namespace WPF_Workshop_MVVM.ViewModels
+{
+    public class StringViewModel : INotifyPropertyChanged
+    {
+        private StringModel model = new StringModel();
+        private string _inputString;
+
+        public string InputString
+        {
+            get => _inputString;
+            set
+            {
+                if (_inputString != value)
+                {
+                    _inputString = value;
+                    OnPropertyChanged(nameof(InputString));
+                    // Automatically update output when input changes
+                    OnPropertyChanged(nameof(OutputString));
+                }
+            }
+        }
+
+        public string OutputString => model.ToUpperCase(InputString);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
